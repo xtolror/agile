f8d14ee07631533965c7184a6e5160cb0aae7853
n3v3rsumm3r <n3v3rsumm3r@gmail.com>
2015-11-05 23:09:28 +0000

    added validation for listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,9 @@
 class Listing < ActiveRecord::Base
     mount_uploader :image, ImageUploader
+    
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :start_bid, presence: true, numericality: true,
+              :format => { :with => /\A\d{1,4}(\.\d{0,2})?\z/ }
+    
 end
