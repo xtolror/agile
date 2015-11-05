56bd713f0817f196427effaf4e2bc6d9d1c820a8
n3v3rsumm3r <n3v3rsumm3r@gmail.com>
2015-11-05 23:33:54 +0000

    fixed length of bid to be longer

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,6 +4,6 @@ class Listing < ActiveRecord::Base
     validates :name, presence: true
     validates :description, presence: true
     validates :start_bid, presence: true, numericality: true,
-              :format => { :with => /\A\d{1,4}(\.\d{0,2})?\z/ }
+              :format => { :with => /\A\d{1,6}(\.\d{0,2})?\z/ }
     
 end
